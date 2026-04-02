53af711033ba8a40c3bac56d3d3aeac72fdff362
alinarogova123-arch <alinarogova123@gmail.com>
2026-04-03 00:06:50 +0300

    Added wines

--- main.py
+++ main.py
@@ -1,4 +1,58 @@
 from http.server import HTTPServer, SimpleHTTPRequestHandler
+from jinja2 import Environment, FileSystemLoader, select_autoescape
+import datetime
+import pandas
+import collections
+
+
+def get_winery_age():
+    foundation_year = datetime.date(year=1920, month=1, day=1)
+    winery_age = datetime.date.today() - foundation_year
+    return int(winery_age.days // 365.25)
+
+
+def get_years_name(year):
+    if year % 100 in (11, 12, 13, 14):
+        return 'лет'
+    elif year % 10 == 1:
+        return 'год'
+    elif year % 10 in (2, 3, 4):
+        return 'года'
+    else:
+        return 'лет'
+
+
+def get_wines_by_category(wines):
+    wines_by_category = collections.defaultdict(list)
+    for wine in wines:
+        wines_by_category[wine.get('Категория')].append(wine)
+    return wines_by_category
+
+
+env = Environment(
+    loader=FileSystemLoader('.'),
+    autoescape=select_autoescape(['html', 'xml'])
+)
+
+template = env.get_template('template.html')
+
+winery_age = get_winery_age()
+
+wines = pandas.read_excel(
+    'wine3.xlsx',
+    na_values='N/A',
+    keep_default_na=False
+).to_dict(orient='records')
+
+rendered_page = template.render(
+    winery_age=winery_age,
+    years_name=get_years_name(winery_age),
+    wines_by_category=get_wines_by_category(wines),
+)
+
+
+with open('index.html', 'w', encoding="utf8") as file:
+    file.write(rendered_page)
 
 
 server = HTTPServer(('0.0.0.0', 8000), SimpleHTTPRequestHandler)
